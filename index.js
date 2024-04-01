5932f806402c5de40267e8206070bf0c269d2b77
wikgu <wikgu@op.pl>
2024-04-01 13:10:02 +0200

    Error handling

--- index.js
+++ index.js
@@ -3,9 +3,15 @@ import https from "https";
 export default (url) => {
   let body = [];
   return new Promise((resolve, reject) => {
-    https.request(url, res=>{
-      res.on('data', chunk=>body.push(chunk));
-      res.on('end', ()=>resolve(eval(Buffer.concat(body).toString())));
-    }).end();
+    try {
+      https
+        .request(url, (res) => {
+          res.on("data", (chunk) => body.push(chunk));
+          res.on("end", () => resolve(eval(Buffer.concat(body).toString())));
+        })
+        .end();
+    } catch (error) {
+      reject(error);
+    }
   });
-}
+};
